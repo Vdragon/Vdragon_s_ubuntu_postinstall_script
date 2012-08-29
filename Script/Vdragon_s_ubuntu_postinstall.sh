4ed800d24fabf3973d22f6b23a9d5a7febc0006b
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-08-29 17:31:24 +0800

    新增DeaDBeeF音樂播放軟體

--- Script/Vdragon_s_ubuntu_postinstall.sh
+++ Script/Vdragon_s_ubuntu_postinstall.sh
@@ -327,6 +327,16 @@ echo -e ${message_update_cache}
 ${command_gain_privilege} ${command_update_source_cache} >> update_cache.log
 ${command_gain_privilege} ${command_install_software} dropbox
 
+echo -e "
+=======================================
+${message_install_or_update} DeaDBeeF音樂播放軟體
+======================================="
+echo -e ${message_add_software_source}
+${command_gain_privilege} ${command_add_software_source} ppa:alexey-smirnov/deadbeef
+echo -e ${message_update_cache}
+${command_gain_privilege} ${command_update_source_cache} >> update_cache.log
+${command_gain_privilege} ${command_install_software} deadbeef
+
 #=====不需要新增軟體來源的軟體=====
 echo -e "
 =======================================
