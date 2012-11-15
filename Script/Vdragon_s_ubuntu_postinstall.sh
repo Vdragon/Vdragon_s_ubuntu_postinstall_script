01b6dae68551ed8e5d986084ced7cd8a7391fe98
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-11-15 21:22:37 +0800

    Changed Kubuntu release PPA to ppa:kubuntu-ppa/backports the correct one.
    fixes #44

--- Script/Vdragon_s_ubuntu_postinstall.sh
+++ Script/Vdragon_s_ubuntu_postinstall.sh
@@ -285,7 +285,7 @@ echo -e "
 ${message_install_or_update} K桌面環境(K Desktop Environment, KDE)軟體組合
 ======================================="
 echo -e ${message_add_software_source}
-${command_gain_privilege} ${command_add_software_source} ppa:kubuntu-ppa/ppa
+${command_gain_privilege} ${command_add_software_source} ppa:kubuntu-ppa/backports
 echo -e ${message_update_cache}
 ${command_gain_privilege} ${command_update_source_cache} >> update_cache.log
 ${command_gain_privilege} ${command_install_software} kde-standard kdesudo kde-l10n-zhtw kdesdk-dolphin-plugins gtk2-engines-oxygen gtk3-engines-oxygen bluedevil kcron kde-config-gtk kde-config-tablet kde-config-telepathy-accounts kde-config-touchpad kdenetwork-filesharing system-config-printer-kde printer-applet
