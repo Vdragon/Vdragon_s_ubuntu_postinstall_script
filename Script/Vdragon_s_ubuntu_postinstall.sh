22f43832df3fa26a07e313fc89c75d45d1d92c1d
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-11-15 21:29:27 +0800

    Add CDRTools, fixes #43

--- Script/Vdragon_s_ubuntu_postinstall.sh
+++ Script/Vdragon_s_ubuntu_postinstall.sh
@@ -332,6 +332,16 @@ echo -e ${message_update_cache}
 ${command_gain_privilege} ${command_update_source_cache} >> update_cache.log
 ${command_gain_privilege} ${command_install_software} xbmc
 
+echo -e "
+=======================================
+${message_install_or_update} CDRTools 光碟燒錄相關工具
+======================================="
+echo -e ${message_add_software_source}
+${command_gain_privilege} ${command_add_software_source} ppa:brandonsnider/cdrtools
+echo -e ${message_update_cache}
+${command_gain_privilege} ${command_update_source_cache} >> update_cache.log
+${command_gain_privilege} ${command_install_software} cdrtools
+
 #=====不需要新增軟體來源的軟體=====
 echo -e "
 =======================================
